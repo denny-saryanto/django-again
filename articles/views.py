c1b88c22b014c2e8db4491d1914e1185563cb680
denny-saryanto <dennysaryanto7@gmail.com>
2022-03-23 14:45:08 +0700

    update

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def index(request):
+    html_string = """ <h1>Halo</h1> """
+    return HttpResponse(html_string)
+
+def showId(request, id):
+    print(id)
+    return HttpResponseRedirect(reverse('index'))
