843f6b336bb5650da218330dfec2bd10b1122172
denny-saryanto <dennysaryanto7@gmail.com>
2022-03-24 10:27:50 +0700

    Search Method

--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,48 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Article
+from djangomid import views as viewsDjangomid
 
 # Create your views here.
 def index(request):
-    html_string = """ <h1>Halo</h1> """
-    return HttpResponse(html_string)
+    return HttpResponseRedirect(reverse(viewsDjangomid.index))
 
-def showId(request, id):
-    article_id = Article.objects.get(id=id)
-    return HttpResponse(""" <center> <h1> {} </h1> <p> {} </p> </center>""".format(article_id.title, article_id.content))
+def showId(request, id=None):
+    try:
+        article_id = Article.objects.get(id=id)
+        context = {
+            'article' : article_id,
+        }
+        return render(request, "articles/index.html", context=context)
+    except Exception:
+        return HttpResponseRedirect(reverse(viewsDjangomid.index))
+
+def searchArticle(request):
+    query = request.GET
+    query_data = query.get('query')
+
+    try:
+        query_data = int(query_data)
+    except:
+        query_data = None
+
+    article, context = None, None
+    if query_data is not None:
+        try:
+            article = Article.objects.get(id=query_data)
+            context = {
+                'result' : {
+                    'id' : article.id,
+                    'title' : article.title,
+                    'content' : article.content,
+                } 
+            }
+        except Exception as e:
+            context = {
+                'result' : {
+                    'id' : "Not Found",
+                    'title' : "Not Found",
+                    'content' : "Not Found",
+                } 
+            }
+    return render(request, 'articles/search.html', context=context)
