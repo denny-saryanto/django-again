f1ad53eb0b1ab1f54633b94f4a720fb14c590e1a
denny-saryanto <dennysaryanto7@gmail.com>
2022-04-06 11:01:55 +0700

    Make slug Unique using Random Numeric

--- articles/models.py
+++ articles/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.utils import timezone
 from django.utils.text import slugify
+import random
+from .utils import slugify_instance_title
 
 # Create your models here.
 class Article(models.Model):
     # https://docs.djangoproject.com/en/2.2/ref/models/fields/#field-types
     # Django model-field-types
     title = models.CharField(max_length=120)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -23,25 +25,13 @@ class Article(models.Model):
         # obj.save()
         # do another something
 
-def slugify_instance_title(instance, save=False):
-    slug = slugify(instance.title)
-    qs = Article.objects.filter(slug=slug).exclude(id=instance.id)
-    if qs.exists():
-        slug = f'{slug}-{instance.id}'
-    instance.slug = slug
-    if save:
-        instance.save()
-    return instance
-
 def article_pre_save(sender, instance, *args, **kwargs):
-    print('pre_save')
     if instance.slug is None:
         slugify_instance_title(instance, save=False)
 
 pre_save.connect(article_pre_save, sender=Article)
 
 def article_post_save(sender, instance, created, *args, **kwargs):
-    print('post_save')
     if created:
         slugify_instance_title(instance, save=True)
 
